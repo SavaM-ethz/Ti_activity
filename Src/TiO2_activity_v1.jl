65c98dd5a5a777905dc9ff2928ea1ecf6fd5b7c5
Lorenzo Candioti <lorenzo.candioti@bluewin.ch>
2024-10-03 19:29:53 +0200

    Added thermodynamic funcs and restructured main scripts.

--- Src/TiO2_activity_v1.jl
+++ Src/TiO2_activity_v1.jl
@@ -1,41 +1,63 @@
-using Plots, MAGEMin_C, Pkg
+using Plots, MAGEMin_C, Pkg, JLD2
 Pkg.activate(".")
 
+# ------------------------- #
+#|  Include functionality  |#
+# ------------------------- #
+include("../Funcs/thermodynamic_functionality.jl")
+
+# ------------------------- #
+#|  Define Main function   |#
+# ------------------------- #
 function main()
-#    data    = Initialize_MAGEMin("ig", verbose=false);
-#    P,T     = 10.0, 1100.0
-#   X= [48.43; 15.19; 11.57; 10.13; 6.65; 1.64; 0.59; 1.87; 0.68; 0.0; 3.0];
-#    Xoxides = ["SiO2"; "Al2O3"; "CaO"; "MgO"; "FeO"; "Fe2O3"; "K2O"; "Na2O"; "TiO2"; "Cr2O3"; "H2O"];
-#    sys_in  = "wt"
-#   out     = single_point_minimization(P, T, data, X=X, Xoxides=Xoxides, sys_in=sys_in)
-#   Finalize_MAGEMin(data)
 
+    # Switches
+    call_MAGEMin        = false      # Calculate titanium activity
+    postprocess_MAGEMin = true     # Postprocess data
 
-    db      = "ig"  # database: ig, igneous (Holland et al., 2018); mp, metapelite (White et al 2014b)
-    data    = Initialize_MAGEMin(db, buffer="qfm", verbose=false);
-    nT      = 50
-    P_const = 2.0
-    B_const = -2.0
-    P       = [P_const for _ in 1:nT]
-    B       = [B_const for _ in 1:nT]
-    T       = collect(LinRange(600.0, 1000.0, nT))
-    X       = [69.3; 15.6; 1.65; 1.18; 2.50; 1.5; 4.61; 3.49; 0.38; 0.0; 4.0];
-    Xoxides = ["SiO2"; "Al2O3"; "CaO"; "MgO"; "FeO"; "O"; "K2O"; "Na2O"; "TiO2"; "Cr2O3"; "H2O"];
-    sys_in  = "wt"
-    out     = multi_point_minimization(P,T, data, X=X, B=B, Xoxides=Xoxides, sys_in=sys_in);
-    Finalize_MAGEMin(data)
+    # Initialization
+    out      = []
+    nT       = 50
+    T_plot   = Vector{Float64}(undef,nT)
+    aTi_plot = Vector{Float64}(undef,nT)
 
-    println("Size of out=$(size(out))")
+    # Calculate TiO2-activity
+    if call_MAGEMin
 
-    T_plot = Vector{Float64}(undef,length(out))
-    aTi_plot = Vector{Float64}(undef,length(out)) 
+        # Prepare & run MAGEMin calculation
+        db      = "ig"  # database: ig, igneous (Holland et al., 2018); mp, metapelite (White et al 2014b)
+        data    = Initialize_MAGEMin(db, buffer="qfm", verbose=false);
+        P_const = 2.0
+        B_const = -2.0
+        P       = [P_const for _ in 1:nT]
+        B       = [B_const for _ in 1:nT]
+        T       = collect(LinRange(600.0, 1000.0, nT))
+        X       = [69.3; 15.6; 1.65; 1.18; 2.50; 1.5; 4.61; 3.49; 0.38; 0.0; 4.0];
+        Xoxides = ["SiO2"; "Al2O3"; "CaO"; "MgO"; "FeO"; "O"; "K2O"; "Na2O"; "TiO2"; "Cr2O3"; "H2O"];
+        sys_in  = "wt"
+        out     = multi_point_minimization(P,T, data, X=X, B=B, Xoxides=Xoxides, sys_in=sys_in);
+        Finalize_MAGEMin(data)
 
-    for idx_out in eachindex(out)
-        T_plot[idx_out] = out[idx_out].T_C
-        aTi_plot[idx_out] = out[idx_out].aTiO2    
+        # Save results
+        JLD2.save("./Data/MAGEMin_out_struct.jld2", "out", out)
     end
-    display(plot(T_plot, aTi_plot, seriestype=:scatter))
-    return out
+
+    # Postprocess data structure
+    if postprocess_MAGEMin
+        out = JLD2.load("./Data/MAGEMin_out_struct.jld2", "out")
+        println(size(out))
+        for idx_out in eachindex(out)
+            T_plot[idx_out]   = out[idx_out].T_C
+            aTi_plot[idx_out] = out[idx_out].aTiO2
+        end
+
+        # Visualize
+        display(plot(T_plot, aTi_plot, seriestype = :scatter))
+    end
+
+    # Return
+    return nothing
+
 end
 
 out=main()
